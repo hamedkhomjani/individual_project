819df1e5cd6b00a76b86a75c2e769a35ac500658
hamedkhomjani <eng.h.khomjani@gmail.com>
2021-09-11 22:10:33 +0200

    implement social-networks part

--- src/components/header/socials/Socials.jsx
+++ src/components/header/socials/Socials.jsx
@@ -1,9 +1,25 @@
 import React from 'react'
+
+import { ReactComponent as FacebookIcon } from '../../../assets/icons/facebook.svg';
+import { ReactComponent as InstgramIcon } from '../../../assets/icons/instagram.svg';
+import { ReactComponent as TwitterIcon } from '../../../assets/icons/twitter.svg';
+
+
 import './Socials.css'
 function Channels() {
     return (
         <div className='socials-network'>
-            Socials
+           
+            <a className='socials-icon'>
+                <FacebookIcon/>
+            </a>
+            <a className='socials-icon'>
+                <InstgramIcon />
+            </a>
+            <a className='socials-icon'>
+                <TwitterIcon />
+            </a>
+
         </div>
     )
 }
