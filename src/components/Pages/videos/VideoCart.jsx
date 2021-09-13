2485b3d45da3538b0465b7a95fce503b512d7ee9
hamedkhomjani <eng.h.khomjani@gmail.com>
2021-09-13 16:14:14 +0200

    create video data mock

--- src/components/Pages/videos/VideoCart.jsx
+++ src/components/Pages/videos/VideoCart.jsx
@@ -6,9 +6,9 @@ function VideoCart(props) {
     return (
         <div className='vide-cart-container'>
             <video controls loop height="150">
-                <source src={`../../../assets/videos/${props.item.image}`} type="video/mp4"/>
+                <source src={`../../../assets/videos/${props.item.video}`} type="video/mp4"/>
             </video>
-            <p>{props.item.description}</p>
+            <p>{props.item.introduction}</p>
         </div>
     )
 }
---
+++ src/components/mocks/PaintsMock.js
@@ -0,0 +1,38 @@
+export const VideosMockData = [
+    {
+      id: '1',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+      image: '1.mp4',
+      artist: 'Alexander Opanasenko',
+      type: 'Oil Paint',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra mauris laoreet blandit semper. Fusce fringilla mi nisi, eu dapibus elit vulputate ut. Vestibulum aliquam venenatis euismod. Integer quis urna nec est interdum maximus.',
+      year: '2014'
+    },
+    {
+      id: '2',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+      image: '2.mp4',
+      artist: 'Alexander Opanasenko',
+      type: 'Oil Paint',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra mauris laoreet blandit semper. Fusce fringilla mi nisi, eu dapibus elit vulputate ut. Vestibulum aliquam venenatis euismod. Integer quis urna nec est interdum maximus.',
+      year: '2014'
+    },
+    {
+      id: '3',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+      image: '3.mp4',
+      artist: 'Alexander Opanasenko',
+      type: 'Oil Paint',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra mauris laoreet blandit semper. Fusce fringilla mi nisi, eu dapibus elit vulputate ut. Vestibulum aliquam venenatis euismod. Integer quis urna nec est interdum maximus.',
+      year: '2014'
+    },
+    {
+      id: '4',
+      name: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. ',
+      image: '4.mp4',
+      artist: 'Alexander Opanasenko',
+      type: 'Oil Paint',
+      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra mauris laoreet blandit semper. Fusce fringilla mi nisi, eu dapibus elit vulputate ut. Vestibulum aliquam venenatis euismod. Integer quis urna nec est interdum maximus.',
+      year: '2014'
+    },
+  ];
