1170f6cfdd5bb3782a06cacd8df691c62feacd38
hamedkhomjani <eng.h.khomjani@gmail.com>
2021-09-13 21:15:32 +0200

    change about

--- src/components/Pages/About/About.jsx
+++ src/components/Pages/About/About.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 
+import personal from '../../../assets/images/personal_image.png';
+import './About.css'
+
 function About() {
     return (
-        <div>
-            About
+        <div className='about-container'>
+            <img src={personal} width='150' height='150' />
+            <h1 id='about-name'>Alexander Opanasenko</h1>
+            <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras pharetra mauris laoreet blandit semper. Fusce fringilla mi nisi, eu dapibus elit vulputate ut. Vestibulum aliquam venenatis euismod. Integer quis urna nec est interdum maximus.
+            </p>
+            <a className='about-learn-more'>LEARN MORE ABOUT THIS ARTIST</a>
         </div>
     )
 }
