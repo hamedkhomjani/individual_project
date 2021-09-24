f95df56365981eb6ab6dd94fded7a4c482a7931a
hamedkhomjani <eng.h.khomjani@gmail.com>
2021-09-24 19:07:28 +0200

    move car

--- src/components/Pages/homePage/HomePage.jsx
+++ src/components/Pages/homePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
 import './HomePage.css'
@@ -6,13 +6,21 @@ import './HomePage.css'
 import car from '../../../assets/images/home_car.png'
 
 function HomePage() {
+    const [move, setMove] = useState(false);
+
+    useEffect(() => {
+        const moveCar = () => setMove(!move);
+        moveCar();
+    }, [])
+
+    
     return (
         <div className='home-container'>
                 <div className='home-content'>
                     <p>Discover Arts of Cars' world with CarArt</p>
                     <Link to='/gallery'><button type="button">More</button></Link>
                 </div>
-                <img src={car} alt="car" className='home-car-image'/>
+                <img src={car} alt="car" className={move ? 'home-car-image active' : 'home-car-image'}/>
         </div>
     )
 }
